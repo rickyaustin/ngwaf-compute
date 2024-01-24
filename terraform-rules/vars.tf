cd3de1a881690401e02495781c715e9192a090b8
rickyaustin <82475896+rickyaustin@users.noreply.github.com>
2024-01-24 10:40:18 -0500

    Update vars.tf

--- terraform-rules/vars.tf
+++ terraform-rules/vars.tf
@@ -1,20 +1,16 @@
-variable "sigsci_token" {
+variable "NGWAF_TOKEN" {
     type = string
-    #default = "de9f1511-5c6a-4368-82bf-161fee0e33cd"
 }
 
-variable "sigsci_email" {
+variable "NGWAF_EMAIL" {
     type = string
-    #default = "raustin+fastly@fastly.com"
 }
 
-variable "sigsci_corp" {
+variable "NGWAF_CORP" {
     type = string
-    default = "ricky_austin"
-
 }
 
-variable "sigsci_site" {
+variable "NGWAF_SITE" {
     type = string
 }
 
